d835b324672c802e3b7d06e280cab851a0c61b4d
UmairCH5 <umairch.qa@gmail.com>
2025-07-21 18:52:08 +0500

    Update Examples.cy.js

--- cypress/e2e/Examples.cy.js
+++ cypress/e2e/Examples.cy.js
@@ -2,36 +2,36 @@ describe('Example test', () =>{
     beforeEach(() => {
         cy.visit('/examples')
     })
-    it('Multi page testing', () => {
-        cy.get('[data-test="nav-why-cypress"]').click()
-        cy.location("pathname").should("equal", "/")
-        cy.wait(1000)
-        cy.get('[data-test="nav-overview"]').click()
-        cy.location("pathname").should("equal", "/overview")
-        cy.wait(1000)
-        cy.get('[data-test="nav-fundamentals"]').click()
-        cy.location("pathname").should("equal", "/fundamentals")
-        cy.wait(1000)
-        cy.get('[data-test="nav-forms"]').click()
-        cy.location("pathname").should("equal", "/forms")
-        cy.wait(1000)
-        cy.get('[data-test="nav-component"]').click()
-        cy.location("pathname").should("equal", "/component")
-        cy.wait(1000)
-        cy.get('[data-test="nav-best-practices"]').click()
-        cy.location("pathname").should("equal", "/best-practices")
-    })
+    // it('Multi page testing', () => {
+    //     cy.get('[data-test="nav-why-cypress"]').click()
+    //     cy.location("pathname").should("equal", "/")
+    //     cy.wait(1000)
+    //     cy.get('[data-test="nav-overview"]').click()
+    //     cy.location("pathname").should("equal", "/overview")
+    //     cy.wait(1000)
+    //     cy.get('[data-test="nav-fundamentals"]').click()
+    //     cy.location("pathname").should("equal", "/fundamentals")
+    //     cy.wait(1000)
+    //     cy.get('[data-test="nav-forms"]').click()
+    //     cy.location("pathname").should("equal", "/forms")
+    //     cy.wait(1000)
+    //     cy.get('[data-test="nav-component"]').click()
+    //     cy.location("pathname").should("equal", "/component")
+    //     cy.wait(1000)
+    //     cy.get('[data-test="nav-best-practices"]').click()
+    //     cy.location("pathname").should("equal", "/best-practices")
+    // })
     it('intercept test', () => {
         cy.intercept('post', 'http://localhost:3000/examples'), {
             fixture: 'example.json'
         }
         cy.get('[test-data="post-button"]').click()
     })
-    // it('Add grudges', () => {
-    //     cy.contains(/Add Some Grudges/i)
-    //     cy.get('[data-test="grudge-list"]').within(()=>{
-    //         cy.get('li').should('have.length', 0)
-    //     })
+    it('Add grudges', () => {
+        cy.contains(/Add Some Grudges/i)
+        cy.get('[data-test="grudge-list"]').within(()=>{
+            cy.get('li').should('have.length', 0)
+        })
         cy.get('[data-test= "clear-grudge"]').should('not.exist')
         cy.get('[data-test= "grudge-title"]').should('have.text', 'Add Some Grudges')
         cy.get('[data-test="grudge-input"]').type('Add first grudge')
